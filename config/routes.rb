a414a2fa0bee765cd5dbfc3aa2b96dfad915a58e
Leslie Hoare <iam@lesleh.co.uk>
2018-05-08 12:08:16 +0100

    Add playground controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  get 'playground' => 'playground#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
