a495d1f4259bb08711f10e772cf2fe5cc41f6931
Leslie Hoare <iam@lesleh.co.uk>
2018-05-11 13:56:19 +0100

    Add caching for photos page

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,19 @@
 class PhotosController < ApplicationController
   def index
-    @photos = Photo.descending
+    @photos = get_photos
+  end
+
+  private
+
+  def get_photos
+    photos = Photo.descending
+    Rails.cache.fetch(photos) do
+      photos.map do |photo|
+        {
+          photo: photo,
+          thumbnail: photo.thumbnail(size: photo.thumbnail_size.to_s)
+        }
+      end
+    end
   end
 end
