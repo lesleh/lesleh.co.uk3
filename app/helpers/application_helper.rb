51e17e75f9819d7193d9391d909cf5ac4a31683f
Leslie Hoare <iam@lesleh.co.uk>
2018-05-27 14:40:21 +0100

    Add helper for setting page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,10 @@ module ApplicationHelper
     "#%06x" % (rand * 0xffffff)
   end
 
+  def page_title_tag
+    title = 'Lesleh.co.uk'
+    title = "#{@page_title} - #{title}" if @page_title.present?
+    content_tag :title, title
+  end
+
 end
