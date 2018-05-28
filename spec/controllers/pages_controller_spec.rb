0700f59605a788e9614dfb4406314fd1b9673794
Leslie Hoare <iam@lesleh.co.uk>
2018-05-28 20:49:48 +0100

    Fix Rubocop offenses

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
-
-  describe "GET #about" do
-    it "returns http success" do
+  describe 'GET #about' do
+    it 'returns http success' do
       get :view, params: { id: 'about' }
       expect(response).to have_http_status(:success)
     end
   end
-
 end
