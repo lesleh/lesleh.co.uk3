fe47068d6ae1d67bd5523def232bf48e6dd5857d
Leslie Hoare <iam@lesleh.co.uk>
2018-05-28 21:18:03 +0100

    Add test Recaptcha keys for testing environment

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,7 +1,10 @@
 # # config/initializers/recaptcha.rb
 Recaptcha.configure do |config|
-  config.public_key  = Rails.application.credentials.recaptcha[:public_key]
-  config.private_key = Rails.application.credentials.recaptcha[:private_key]
-  # Uncomment the following line if you are using a proxy server:
-  # config.proxy = 'http://myproxy.com.au:8080'
+  if Rails.application.credentials.recaptcha
+    config.public_key  = Rails.application.credentials.recaptcha[:public_key]
+    config.private_key = Rails.application.credentials.recaptcha[:private_key]
+  else
+    config.public_key  = '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'
+    config.private_key = '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'
+  end
 end
