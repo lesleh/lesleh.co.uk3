0429edd1bc7096f85436405bab4e1684aa54662c
Leslie Hoare <iam@lesleh.co.uk>
2018-05-28 20:13:48 +0100

    Update about page spec to pass in correct ID

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe PagesController, type: :controller do
 
   describe "GET #about" do
     it "returns http success" do
-      get :about
+      get :view, params: { id: 'about' }
       expect(response).to have_http_status(:success)
     end
   end
