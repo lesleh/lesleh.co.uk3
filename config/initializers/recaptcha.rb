aa0ab4098592657a9b0567e9f6763cfb42f30d52
Leslie Hoare <iam@lesleh.co.uk>
2018-09-04 14:36:12 +0100

    Upgrade all gems

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,10 +1,10 @@
 # # config/initializers/recaptcha.rb
 Recaptcha.configure do |config|
   if Rails.application.credentials.recaptcha
-    config.public_key  = Rails.application.credentials.recaptcha[:public_key]
-    config.private_key = Rails.application.credentials.recaptcha[:private_key]
+    config.site_key  = Rails.application.credentials.recaptcha[:public_key]
+    config.secret_key = Rails.application.credentials.recaptcha[:private_key]
   else
-    config.public_key  = '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'
-    config.private_key = '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'
+    config.site_key  = '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'
+    config.secret_key = '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'
   end
 end
