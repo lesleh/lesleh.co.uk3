9c12e47014f61601ba9fc779b9f96a308aa1ac4b
Leslie Hoare <leslie.hoare@simplybusiness.co.uk>
2018-10-06 13:53:39 +0100

    Replace hamburger with FontAwesome icon

--- webpack/javascripts/layout/icons.ts
+++ webpack/javascripts/layout/icons.ts
@@ -1,13 +1,21 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
 import { faGithubAlt } from '@fortawesome/free-brands-svg-icons'
 import {
+  faBars,
   faEnvelope,
   faHome,
   faImages,
   faUserCircle,
 } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faGithubAlt, faEnvelope, faHome, faImages, faUserCircle);
+library.add(
+  faGithubAlt,
+  faBars,
+  faEnvelope,
+  faHome,
+  faImages,
+  faUserCircle
+);
 
 document.addEventListener('turbolinks:load', function () {
   dom.watch()
