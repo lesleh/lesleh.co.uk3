888636b19c0d553ebe1f635e2aa2f0c0ccfad606
Leslie Hoare <leslie.hoare@simplybusiness.co.uk>
2018-10-06 02:34:05 +0100

    Conditionally load lightbox

--- webpack/javascripts/gallery/lightbox.ts
+++ webpack/javascripts/gallery/lightbox.ts
@@ -1,4 +1,7 @@
 document.addEventListener('turbolinks:load', async function () {
+  if(!document.querySelector('.glightbox')) {
+    return
+  }
   const GLightbox = await import('glightbox')
   GLightbox.default({
     'selector': 'glightbox',
