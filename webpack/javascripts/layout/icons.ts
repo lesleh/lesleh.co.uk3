68dea40f4659415407ba48158a4db8badab5f81a
Leslie Hoare <leslie.hoare@simplybusiness.co.uk>
2018-10-06 12:31:46 +0100

    Revert some changes

--- webpack/javascripts/layout/icons.ts
+++ webpack/javascripts/layout/icons.ts
@@ -1,13 +1,8 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
 import { faGithubAlt } from '@fortawesome/free-brands-svg-icons'
-import {
-  faEnvelope,
-  faHome,
-  faImages,
-  faUserCircle,
-} from '@fortawesome/free-solid-svg-icons'
 
-library.add(faGithubAlt, faEnvelope, faHome, faImages, faUserCircle);
+library.add(faGithubAlt)
+
 
 document.addEventListener('turbolinks:load', function () {
   dom.watch()
