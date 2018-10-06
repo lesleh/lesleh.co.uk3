10437718cff7558333b16d9882ace0f0ece8199e
Leslie Hoare <leslie.hoare@simplybusiness.co.uk>
2018-10-06 01:17:30 +0100

    Fix icon loading on turbolinks page change

--- webpack/javascripts/layout/icons.ts
+++ webpack/javascripts/layout/icons.ts
@@ -2,4 +2,8 @@ import { library, dom } from '@fortawesome/fontawesome-svg-core'
 import { faGithubAlt } from '@fortawesome/free-brands-svg-icons'
 
 library.add(faGithubAlt)
-dom.watch()
+
+
+document.addEventListener('turbolinks:load', function () {
+  dom.watch()
+})
